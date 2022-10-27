a14ac936d1f142f56dcc8574134f692ab550afc6
RubsRafa <rubiarafaela2000@gmail.com>
2022-10-27 14:05:19 -0300

    versao com layout sem funcionalidades

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-alert ("oi");
+let nome = prompt ("Nome"); 
+
+function entrarServidor () {
+    const usuario = {name:nome}
+    const promessa = axios.post ('https://mock-api.driven.com.br/api/v6/uol/participants ', usuario);
+    promessa.then (addMensagens)
+}
+
+function enviar () {
+    const textoDigitado = document.querySelector (".barra-escrever").value;
+    const chat = document.querySelector ("ul");
+    chat.innerHTML += `<li class="caixa-texto">
+    <div class="texto">
+        <span class="horario">(00:00:00)</span>
+        <span class="nome">${nome}</span>
+        <span class="para"></span>
+        <span class="destinatario"></span>
+        <span class="mensagem">: ${textoDigitado}</span>
+    </div>
+</li>`
+}
+
+function addMensagens () {
+    const resposta = axios.get ('https://mock-api.driven.com.br/api/v6/uol/messages');
+    resposta.then (renderizarConversa)
+    console.log (resposta);
+}
+scrollIntoview (addMensagens);
+addMensagens();
+
+function renderizarConversa (promisse) {
+    const elementoHora = document.querySelector (".horario");
+    const elementoNome = document.querySelector (".nome");
+    const elementoParaQuem = document.querySelector (".destinatario");
+    const elementoMensagem = document.querySelector (".mensagem");
+    
+    elementoHora.innerHTML = promisse.data.time; 
+    elementoNome.innerHTML = promisse.data.from; 
+    elementoParaQuem.innerHTML = promisse.data.to; 
+    elementoMensagem.innerHTML = promisse.data.text; 
+
+    console.log (promisse)
+}
