5eab779b13550f5323bc6a5ed06da68e16932d3b
RubsRafa <rubiarafaela2000@gmail.com>
2022-10-27 22:04:41 -0300

    versao com um pouquinho de JS e API

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
-let nome = prompt ("Nome"); 
-
-function entrarServidor () {
-    const usuario = {name:nome}
-    const promessa = axios.post ('https://mock-api.driven.com.br/api/v6/uol/participants ', usuario);
-    promessa.then (addMensagens)
+let nome = prompt ("Nome?")
+while (nome == '') {
+    alert ("Diz o nome aí velho")
+    nome = prompt ("Nome?");
 }
-
-function enviar () {
-    const textoDigitado = document.querySelector (".barra-escrever").value;
-    const chat = document.querySelector ("ul");
-    chat.innerHTML += `<li class="caixa-texto">
-    <div class="texto">
-        <span class="horario">(00:00:00)</span>
-        <span class="nome">${nome}</span>
-        <span class="para"></span>
-        <span class="destinatario"></span>
-        <span class="mensagem">: ${textoDigitado}</span>
-    </div>
-</li>`
-}
-
 function addMensagens () {
+    let listaMensagens = []; 
+
+    const promessa = axios.post ('https://mock-api.driven.com.br/api/v6/uol/participants', {name: nome});
+    promessa.then (enviar)
     const resposta = axios.get ('https://mock-api.driven.com.br/api/v6/uol/messages');
     resposta.then (renderizarConversa)
     console.log (resposta);
+
+    for (let i = 0; i < resposta.data.length; i++) {
+        listaMensagens.push (resposta[i]);
+    }
+    return listaMensagens;
+
 }
-scrollIntoview (addMensagens);
 addMensagens();
+
 
 function renderizarConversa (promisse) {
     const elementoHora = document.querySelector (".horario");
@@ -39,5 +32,33 @@ function renderizarConversa (promisse) {
     elementoParaQuem.innerHTML = promisse.data.to; 
     elementoMensagem.innerHTML = promisse.data.text; 
 
+    const addElementos = document.querySelector ('ul');
+    for (let i = 0; i <promisse.length; i++) {
+        addElementos.innerHTML += `<li class="caixa-texto">
+        <div class="texto">
+            <span class="horario">${elementoHora}</span>
+            <span class="nome">${elementoNome}</span>
+            <span class="para">para</span>
+            <span class="destinatario">${elementoParaQuem}:</span>
+            <span class="mensagem">${elementoMensagem}</span>
+        </div>
+    </li>`
+    }
     console.log (promisse)
+}
+
+function enviar () {
+    const texto = document.querySelector ('.input').value;
+    const enviarElemento = document.querySelector ("ul");
+    enviarElemento.innerHTML += `<li class="caixa-texto">
+    <div class="texto">
+        <span class="horario">${elementoHora}</span>
+        <span class="nome">${nome}</span>
+        <span class="para">para</span>
+        <span class="destinatario">${elementoParaQuem}:</span>
+        <span class="mensagem">${texto}</span>
+    </div>
+</li>`
+
+    
 }
