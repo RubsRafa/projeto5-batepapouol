ae34d95503a49d1d9e72dc839c6e10ef2dabadee
RubsRafa <rubiarafaela2000@gmail.com>
2022-10-28 21:40:47 -0300

    versao pegando as mensagens do servidor

--- script.js
+++ script.js
@@ -1,64 +1,110 @@
-let nome = prompt ("Nome?")
-while (nome == '') {
-    alert ("Diz o nome aí velho")
-    nome = prompt ("Nome?");
-}
-function addMensagens () {
-    let listaMensagens = []; 
 
-    const promessa = axios.post ('https://mock-api.driven.com.br/api/v6/uol/participants', {name: nome});
-    promessa.then (enviar)
-    const resposta = axios.get ('https://mock-api.driven.com.br/api/v6/uol/messages');
-    resposta.then (renderizarConversa)
-    console.log (resposta);
-
-    for (let i = 0; i < resposta.data.length; i++) {
-        listaMensagens.push (resposta[i]);
+let nome; 
+function entrar() {
+    nome = document.querySelector ('.nome-escrever').value;
+    const entrada = document.querySelector ('.caixa-entrada');
+    const chat = document.querySelector ('.conteudo');    
+    if (nome !== '') {
+        entrada.classList.add ('esconder');
+        chat.classList.remove ('esconder')
+    } else {
+        return;
     }
-    return listaMensagens;
-
 }
-addMensagens();
 
 
-function renderizarConversa (promisse) {
-    const elementoHora = document.querySelector (".horario");
-    const elementoNome = document.querySelector (".nome");
-    const elementoParaQuem = document.querySelector (".destinatario");
-    const elementoMensagem = document.querySelector (".mensagem");
+const enviar = axios.post ('https://mock-api.driven.com.br/api/v6/uol/participants ', {name: `${nome}`});
+enviar.then (teste); 
+console.log (enviar)
+
+function teste (item) {
+    console.log (item);
+}
+
+//function mandarMensagem (elemento) {
+//    let textoParaEnviar = document.querySelector ('input').value;
+//    console.log (textoParaEnviar);
+//    console.log (elemento)
+//
+//    let usuario = {
+//        from: `${nome}`,
+//        to: 'Todos',
+//        text: textoParaEnviar,
+//        type: 'message'
+//    }
+
+
+//    const enviarElemento = document.querySelector ("ul");
+//    enviarElemento.innerHTML += `<li class="caixa-texto">
+//    <div class="texto">
+//        <span class="horario"></span>
+//        <span class="nome">${usuario.nome}</span>
+//        <span class="para">para</span>
+//        <span class="destinatario">${usuario.to}:</span>
+//        <span class="mensagem">${usuario.text}</span>
+//    </div>
+//    </li>`
+
+//    textoParaEnviar = ''; 
+//
+//    console.log (elemento);
+//    console.log (enviarElemento)
+//}
+///////////////////////
+
+
+
+
+
+
+
+
+
+
+
+const promesa = axios.get ('https://mock-api.driven.com.br/api/v6/uol/messages');
+promesa.then (pegarConversas)
+
+function pegarConversas (promisse) {
+    let mensagens = promisse.data; 
+    let listaMensagem = [];
     
-    elementoHora.innerHTML = promisse.data.time; 
-    elementoNome.innerHTML = promisse.data.from; 
-    elementoParaQuem.innerHTML = promisse.data.to; 
-    elementoMensagem.innerHTML = promisse.data.text; 
+    const addElementos = document.querySelector ('ul'); 
 
-    const addElementos = document.querySelector ('ul');
-    for (let i = 0; i <promisse.length; i++) {
-        addElementos.innerHTML += `<li class="caixa-texto">
-        <div class="texto">
-            <span class="horario">${elementoHora}</span>
-            <span class="nome">${elementoNome}</span>
-            <span class="para">para</span>
-            <span class="destinatario">${elementoParaQuem}:</span>
-            <span class="mensagem">${elementoMensagem}</span>
-        </div>
-    </li>`
+    for (let i = 0; i < mensagens.length; i++) {
+        listaMensagem = mensagens[i];
+        if (listaMensagem.type == 'status') {
+            addElementos.innerHTML += `<li class="caixa-texto type-status">
+            <div class="texto">
+            <span class="horario">(${listaMensagem.time})</span>
+            <span class="nome">${listaMensagem.from}</span>
+            <span class="mensagem">${listaMensagem.text}</span>
+            </div>
+            </li>`
+        } else if (listaMensagem.type == 'message') {
+            addElementos.innerHTML += `<li class="caixa-texto type-message">
+            <div class="texto">
+                <span class="horario">(${listaMensagem.time})</span>
+                <span class="nome">${listaMensagem.from}</span>
+                <span class="para">para</span>
+                <span class="destinatario">${listaMensagem.to}:</span>
+                <span class="mensagem">${listaMensagem.text}</span>
+            </div>
+        </li>`
+        } else if (listaMensagem.type == 'private_message') {
+            addElementos.innerHTML += `<li class="caixa-texto type-private_message">
+            <div class="texto">
+                <span class="horario">(${listaMensagem.time})</span>
+                <span class="nome">${listaMensagem.from}</span>
+                <span class="para">para</span>
+                <span class="destinatario">${listaMensagem.to}:</span>
+                <span class="mensagem">${listaMensagem.text}</span>
+            </div>
+        </li>`
+        }
     }
-    console.log (promisse)
 }
 
-function enviar () {
-    const texto = document.querySelector ('.input').value;
-    const enviarElemento = document.querySelector ("ul");
-    enviarElemento.innerHTML += `<li class="caixa-texto">
-    <div class="texto">
-        <span class="horario">${elementoHora}</span>
-        <span class="nome">${nome}</span>
-        <span class="para">para</span>
-        <span class="destinatario">${elementoParaQuem}:</span>
-        <span class="mensagem">${texto}</span>
-    </div>
-</li>`
-
-    
-}
+const rolarPapo = document.querySelector ('.chat');
+rolarPapo.scrollIntoView();
+setTimeout(pegarConversas, 1000); 
